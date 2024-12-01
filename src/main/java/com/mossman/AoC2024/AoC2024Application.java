93fb8bec81998bbd91257ed95a46381172c571f0
FMossman <fmcmossman@gmail.com>
2024-12-01 08:55:55 +0000

    Initial commit

---
+++ src/main/java/com/mossman/AoC2024/AoC2024Application.java
@@ -0,0 +1,13 @@
+package com.mossman.AoC2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoC2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoC2024Application.class, args);
+	}
+
+}
